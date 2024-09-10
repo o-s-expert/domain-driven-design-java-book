58f607f0b09194eb0ea04a10ec69ac7140e69b0b
Otavio Santana <otaviopolianasantana@gmail.com>
2024-09-10 18:53:36 +0100

    create player builder
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ chapter-03/src/main/java/expert/os/books/ddd/chapter03/player/PlayerBuilder.java
@@ -0,0 +1,53 @@
+package expert.os.books.ddd.chapter03.player;
+
+import java.time.Year;
+
+public class PlayerBuilder {
+
+    private String name;
+
+    private Year start;
+
+    private Year end;
+
+    private String email;
+
+    private Position position;
+
+    private int goal = 0;
+
+
+    public PlayerBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PlayerBuilder start(Year start) {
+        this.start = start;
+        return this;
+    }
+
+    public PlayerBuilder end(Year end) {
+        this.end = end;
+        return this;
+    }
+
+    public PlayerBuilder email(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public PlayerBuilder position(Position position) {
+        this.position = position;
+        return this;
+    }
+
+    public PlayerBuilder goal(int goal) {
+        this.goal = goal;
+        return this;
+    }
+
+    public Player build() {
+        return new Player(name, start, end, email, position);
+    }
+}
