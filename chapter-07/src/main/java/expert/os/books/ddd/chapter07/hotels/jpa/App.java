5712b58bb37c7a95b5ad80161a6fdf96621630b2
Otavio Santana <otaviopolianasantana@gmail.com>
2025-02-02 17:31:27 +0000

    chore: update checkstyle on chapter 07
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-07/src/main/java/expert/os/books/ddd/chapter07/hotels/jpa/App.java
+++ chapter-07/src/main/java/expert/os/books/ddd/chapter07/hotels/jpa/App.java
@@ -1,5 +1,6 @@
 package expert.os.books.ddd.chapter07.hotels.jpa;
 
+import org.hibernate.Session;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -15,7 +16,9 @@ public final class App {
                     .buildMetadata()
                     .buildSessionFactory();
 
-
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.close();
         } catch (Exception e) {
             // The registry would be destroyed by the SessionFactory, but we
             // had trouble building the SessionFactory so destroy it manually.
