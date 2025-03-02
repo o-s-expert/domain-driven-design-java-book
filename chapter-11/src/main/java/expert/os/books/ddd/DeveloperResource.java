bb436b1bf2d28b7769eec68728172d4bcaa1dc07
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-02 14:41:36 +0000

    feat: use developer resource
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-11/src/main/java/expert/os/books/ddd/DeveloperResource.java
+++ chapter-11/src/main/java/expert/os/books/ddd/DeveloperResource.java
@@ -2,5 +2,5 @@ package expert.os.books.ddd;
 
 import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
 
-public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
+public interface DeveloperResource extends PanacheEntityResource<Developer, Long> {
 }
