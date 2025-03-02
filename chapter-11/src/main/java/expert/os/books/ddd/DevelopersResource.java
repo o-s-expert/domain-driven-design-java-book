20ce752155d6a3806693d9a500bf497410cdcfa3
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-02 14:43:13 +0000

    feat: update develoeprs
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-11/src/main/java/expert/os/books/ddd/DevelopersResource.java
+++ chapter-11/src/main/java/expert/os/books/ddd/DevelopersResource.java
@@ -2,5 +2,5 @@ package expert.os.books.ddd;
 
 import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
 
-public interface DeveloperResource extends PanacheEntityResource<Developer, Long> {
+public interface DevelopersResource extends PanacheEntityResource<Developer, Long> {
 }
