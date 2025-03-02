d901b80258c99dc36ac1233fdf2b438697c11685
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-02 08:04:43 +0000

    feat: update dto tohave equals and hashcode
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-10/src/main/java/expert/os/books/ddd/chapter10/products/application/CategoryDTO.java
+++ chapter-10/src/main/java/expert/os/books/ddd/chapter10/products/application/CategoryDTO.java
@@ -4,10 +4,35 @@ package expert.os.books.ddd.chapter10.products.application;
 import expert.os.books.ddd.chapter10.products.infra.FieldVisibilityStrategy;
 import jakarta.json.bind.annotation.JsonbVisibility;
 
+import java.util.Objects;
+
 @JsonbVisibility(FieldVisibilityStrategy.class)
 public class CategoryDTO {
 
     private String name;
 
     private String description;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CategoryDTO that = (CategoryDTO) o;
+        return Objects.equals(name, that.name) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description);
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryDTO{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
--- chapter-10/src/main/java/expert/os/books/ddd/chapter10/products/application/ManufacturerDTO.java
+++ chapter-10/src/main/java/expert/os/books/ddd/chapter10/products/application/ManufacturerDTO.java
@@ -2,7 +2,8 @@ package expert.os.books.ddd.chapter10.products.application;
 
 import expert.os.books.ddd.chapter10.products.infra.FieldVisibilityStrategy;
 import jakarta.json.bind.annotation.JsonbVisibility;
-import jakarta.nosql.Column;
+
+import java.util.Objects;
 
 @JsonbVisibility(FieldVisibilityStrategy.class)
 public class ManufacturerDTO {
@@ -14,5 +15,28 @@ public class ManufacturerDTO {
     private String contactNumber;
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ManufacturerDTO that = (ManufacturerDTO) o;
+        return Objects.equals(name, that.name)
+                && Objects.equals(address, that.address)
+                && Objects.equals(contactNumber, that.contactNumber);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, address, contactNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "ManufacturerDTO{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", contactNumber='" + contactNumber + '\'' +
+                '}';
+    }
 }
